b265edc9e6a12677245706b616a133b4cd29a28e
Spentine <114318049+Spentine@users.noreply.github.com>
2024-05-10 21:32:51 -0700

    working on skins
    should be able to render a board soon

--- render.js
+++ render.js
@@ -1,3 +1,30 @@
+const skins = {}
+
+function addSkin(skinName, skinPaths, connected) {
+  const skin = {};
+  
+  const minoSkin = new Image();
+  minoSkin.src = skinPaths.minos.src;
+  skin.minos = {"src": minoSkin,
+    "connected": skinPaths.minos.connected,
+    "blockSize": skinPaths.minos.blockSize,
+  };
+  
+  const ghostSkin = new Image();
+  ghostSkin.src = skinPaths.ghost.src;
+  skin.ghost = {"src": ghostSkin,
+    "connected": skinPaths.ghost.connected,
+    "blockSize": skinPaths.ghost.blockSize,
+  };
+  
+  skins[skinName] = skin;
+}
+
+addSkin("TETR.IO", {
+  "minos": {"src": "assets/inGame/TETRIOconnectedMinos.png", "connected": true, "blockSize": 96},
+  "ghost": {"src": "assets/inGame/TETRIOconnectedGhost.png", "connected": true, "blockSize": 96},
+});
+
 class GameRenderer {
   constructor() {
     this.canvas = null;
@@ -14,6 +41,12 @@ class GameRenderer {
     this.canvas.height = window.innerHeight;
     this.ctx.fillStyle = "#111";
     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    // this.renderGameBoard(0, "TETR.IO");
+  }
+  
+  renderGameBoard(game, skinName) {
+    const skin = skins[skinName];
+    this.ctx.drawImage(skin.minos.src, 0, 0);
   }
 }
 
